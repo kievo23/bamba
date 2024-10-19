78ed390c0a872da82a8c1bf635811985893a8172
kievo23 <kelvinchege@gmail.com>
2024-10-19 12:04:08 +0300

    add logging to all mpesa api calls

--- src/models/Logging.js
+++ src/models/Logging.js
@@ -3,10 +3,10 @@ import fs from 'node:fs';
 const log = (data) => {
     let currentDate = new Date();
     let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    console.log(daysOfWeek[currentDate.getDay()]+ ", " +currentDate.toLocaleString());
+    //console.log(daysOfWeek[currentDate.getDay()]+ ", " +currentDate.toLocaleString());
 
     let writer = fs.createWriteStream(process.env.LOGGING_DIR, { flag: 'a+' });
-    writer.write(daysOfWeek[currentDate.getDay()]+ ", " +currentDate.toLocaleString() + "\t" +data+'\n');
+    writer.write(daysOfWeek[currentDate.getDay()]+ ", " +currentDate.toLocaleString() + "\t" + JSON.stringify(data)+'\n');
 }
 
 export default log
