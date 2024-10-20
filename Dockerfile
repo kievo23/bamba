b69b9bb9d5be6a1dc4653ab1d9db105369403109
kievo23 <kelvinchege@gmail.com>
2024-10-20 13:56:57 +0300

    changes to cors

--- Dockerfile
+++ Dockerfile
@@ -13,6 +13,8 @@ RUN npm install
 #Bundle app source code
 COPY . .
 
+ENV NODE_ENV=production
+
 EXPOSE 8000
 
 CMD ["npm","start"]
--- src/app.js
+++ src/app.js
@@ -18,21 +18,27 @@ const mpesaOrigin = process.env.MPESA_BASEURL;
 const confirmURL = process.env.ConfirmationMpesaURL;
 
 // Create a CORS options object
-const corsOptions = {
-  origin: (origin, callback) => {
-    console.log(origin);
-    if ([bambaOrigin,mpesaOrigin].includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     console.log(origin);
+//     if ([bambaOrigin,mpesaOrigin].includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   optionsSuccessStatus: 200
+// };
 
 app.use(bodyParser.json());
 app.use(express.static('public'))
-//app.use(cors());
+app.use(cors());
+
+app.use((req, res, next) => {
+  console.log('Request received:', req.method, req.path);
+  console.log('Request headers:', req.headers);
+  next();
+});
 
 app.get("/", (req, res, next) => {
   res.json("Welcome to bamba backend. We are empowering Airtel users to access credit via Mpesa. /sendcredit, /apikey, /to");
@@ -41,15 +47,15 @@ app.get("/", (req, res, next) => {
 app.use('/api', mpesaPurchaseRouter);
 app.use('/test', testsRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
 
-  fs.writeFile(process.env.LOGGING_DIR, "Keeping an eye on MPESA", err => {
-    if (err) {
-      console.error(err);
-    } else {
-      // file written successfully
-    }
-  });
+  // fs.writeFile(process.env.LOGGING_DIR, "Keeping an eye on MPESA", err => {
+  //   if (err) {
+  //     console.error(err);
+  //   } else {
+  //     // file written successfully
+  //   }
+  // });
   console.log(`App running on hazel ${PORT}`);
 });
 
